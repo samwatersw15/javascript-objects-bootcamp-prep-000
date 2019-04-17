59ad7929368c19d4f38fb8f994cde4292970ffa7
Sam Waters <samwatersw15@gmail.com>
2019-04-17 22:06:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,6 @@ var playlist = new Object ( {FrankSinatra:'My Way'} )
 function updatePlaylist(playlist, artistName, songTitle) {
  return Object.assign(playlist, {[artistName]:songTitle})
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete
+}
