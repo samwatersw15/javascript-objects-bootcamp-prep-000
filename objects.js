2a05f5f861d0b2edef5ef6287c6a2921a397b3c8
Sam Waters <samwatersw15@gmail.com>
2019-04-17 22:06:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
  return Object.assign(playlist, {[artistName]:songTitle})
 }
 function removeFromPlaylist(playlist, artistName) {
-  delete
+  delete playlist.artistName
 }
